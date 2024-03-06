1a099c3b2a9b4ddd8684933db5cdf89434fe4e8a
Patrick <p@p>
2024-03-06 08:54:16 +0000

    refactored to compose

--- app/src/main/java/com/lbu/explicit_intents/MainActivity.kt
+++ app/src/main/java/com/lbu/explicit_intents/MainActivity.kt
@@ -1,62 +1,63 @@
 package com.lbu.explicit_intents
 
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.EditText
-import android.widget.TextView
-import android.os.Bundle
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.lbu.explicit_intents.ui.theme.ExplicitIntentTheme
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var mMessageEditText: EditText
-    private lateinit var mReplyHeadTextView: TextView
-    private lateinit var mReplyTextView: TextView
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            ExplicitIntentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainPage()
+                }
+            }
+        }
+
         Log.d(LOG_TAG, "-------")
         Log.d(LOG_TAG, "onCreate")
-        mMessageEditText = findViewById(R.id.editText_main)
-        mReplyHeadTextView = findViewById(R.id.text_header_reply)
-        mReplyTextView = findViewById(R.id.text_message_reply)
-        if (savedInstanceState != null) {
-            val isVisible = savedInstanceState
-                .getBoolean("reply_visible")
-            if (isVisible) {
-                mReplyHeadTextView.setVisibility(View.VISIBLE)
-                mReplyTextView.setText(
-                    savedInstanceState
-                        .getString("reply_text")
-                )
-                mReplyTextView.setVisibility(View.VISIBLE)
-            }
-        }
-    }
-
-    fun launchSecondActivity(view: View?) {
-        Log.d(LOG_TAG, "Button clicked!")
-        val message = mMessageEditText.text.toString()
-        val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra(EXTRA_MESSAGE, message)
-        startActivityForResult(intent, TEXT_REQUEST)
-        //startActivity(intent);
-    }
-
-    @Deprecated("Deprecated in Java")
-    public override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int, data: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                val reply = data!!.getStringExtra(SecondActivity.EXTRA_REPLY)
-                mReplyHeadTextView.visibility = View.VISIBLE
-                mReplyTextView.text = reply
-                mReplyTextView.visibility = View.VISIBLE
-            }
-        }
     }
 
     override fun onStart() {
@@ -89,17 +90,114 @@ class MainActivity : AppCompatActivity() {
         Log.d(LOG_TAG, "onDestroy")
     }
 
-    public override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        if (mReplyHeadTextView.visibility == View.VISIBLE) {
-            outState.putBoolean("reply_visible", true)
-            outState.putString("reply_text", mReplyTextView.text.toString())
-        }
-    }
-
     companion object {
         private val LOG_TAG = MainActivity::class.java.simpleName
         const val EXTRA_MESSAGE = "com.lbu.explicit_intents.MainActivity.extra.MESSAGE"
-        const val TEXT_REQUEST = 6876
     }
 }
+
+fun createToast(context: Context) {
+    Toast.makeText(context, R.string.back_button_pressed, Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+fun MainPage(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val input: MutableState<String> = rememberSaveable { mutableStateOf("")}
+    val replyMessage: MutableState<String> = rememberSaveable { mutableStateOf("")}
+    var isVisible : MutableState<Boolean> = rememberSaveable {mutableStateOf(false)}
+
+    val startForResult =
+        rememberLauncherForActivityResult(ActivityResultContracts
+                .StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                replyMessage.value = intent!!.getStringExtra(SecondActivity.EXTRA_REPLY).toString()
+                isVisible.value = true;
+            }
+            else {
+                isVisible.value = false;
+                createToast(context = context)
+            }
+        }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Gray),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        TopBox(visibility = isVisible.value, replyMessage = replyMessage, modifier = modifier)
+        BottomBox(input = input, activityForResult = startForResult, context = context, modifier = modifier)
+
+    }
+}
+
+@Composable
+fun TopBox(modifier: Modifier = Modifier, replyMessage: MutableState<String>, visibility: Boolean) {
+    val height:Int = if(visibility) 120 else 0
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(height.dp)
+            .background(color = Color.Yellow),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        if(visibility) {
+            Column() {
+                Text(
+                    text = stringResource(id = R.string.text_header_reply),
+                    fontSize = 32.sp,
+                )
+                Text(
+                    text = replyMessage.value,
+                    fontSize = 16.sp,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomBox(
+    modifier: Modifier = Modifier,
+    activityForResult: ManagedActivityResultLauncher<Intent, ActivityResult>,
+    context: Context,
+    input: MutableState<String>
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .background(color = Color.Yellow),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        Row() {
+            TextField(value = input.value,
+                onValueChange = { it ->
+                    input.value = it
+                }
+            )
+            Button(onClick = {
+                val message = input.value.toString()
+                val intent = Intent(context, SecondActivity::class.java)
+                intent.putExtra(MainActivity.EXTRA_MESSAGE, message)
+                activityForResult.launch(intent)
+            }) {
+                Text(
+                    text = "SEND",
+                    fontSize = 16.sp,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun IntentsPreview() {
+    ExplicitIntentTheme {
+        MainPage()
+    }
+}
+
+
--- app/src/main/java/com/lbu/explicit_intents/SecondActivity.kt
+++ app/src/main/java/com/lbu/explicit_intents/SecondActivity.kt
@@ -1,33 +1,137 @@
 package com.lbu.explicit_intents
 
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.EditText
-import android.os.Bundle
+import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
-import android.widget.TextView
-import android.view.View
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.lbu.explicit_intents.ui.theme.ExplicitIntentTheme
 
-class SecondActivity : AppCompatActivity() {
-    private var mReply: EditText? = null
+class SecondActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
-        mReply = findViewById(R.id.editText_second)
         val intent = intent
-        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
-        val textView = findViewById<TextView>(R.id.text_message)
-        textView.text = message
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE).toString()
+        setContent {
+            ExplicitIntentTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ReplyPage(message = message)
+                }
+            }
+        }
     }
-
-    fun returnReply(view: View?) {
-        val reply = mReply!!.text.toString()
-        val replyIntent = Intent()
-        replyIntent.putExtra(EXTRA_REPLY, reply)
-        setResult(RESULT_OK, replyIntent)
-        finish()
-    }
-
     companion object {
         const val EXTRA_REPLY = "com.lbu.explicit_intents.SecondActivity.extra.REPLY"
+    }
+}
+
+@Composable
+fun ReplyPage(modifier: Modifier = Modifier, message: String) {
+    val context = LocalContext.current
+    val inputReply: MutableState<String> = rememberSaveable { mutableStateOf("") }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Gray),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        TopReplyBox(sentMessage = message, modifier = modifier)
+        BottomReplyBox(inputReply = inputReply)
+
+    }
+}
+@Composable
+fun TopReplyBox(modifier: Modifier = Modifier, sentMessage:String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .background(color = Color.Yellow),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+            Column() {
+                Text(
+                    text = stringResource(id = R.string.text_header),
+                    fontSize = 32.sp,
+                )
+                Text(
+                    text = sentMessage,
+                    fontSize = 16.sp,
+                )
+            }
+    }
+}
+
+@Composable
+fun BottomReplyBox(
+    inputReply: MutableState<String>
+) {
+    val activity = (LocalContext.current as? Activity)
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .background(color = Color.Yellow),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        Row() {
+            TextField(value = inputReply.value,
+                onValueChange = { it ->
+                    inputReply.value = it
+                }
+            )
+            Button(onClick = {
+                val message = inputReply.value
+                val intent = Intent()
+                intent.putExtra(SecondActivity.EXTRA_REPLY, message)
+                activity?.setResult(RESULT_OK, intent)
+                activity?.finish()
+            }) {
+                Text(
+                    text = "REPLY",
+                    fontSize = 16.sp,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ReplyPreview() {
+    ExplicitIntentTheme {
+        ReplyPage(message = "TEST STRING")
     }
 }
